e76a387f0cf5299fbc6fd6c63e407bf53405fd95
tenda <tenda@localhost.localdomain>
2025-06-10 14:43:44 +0900

    ルート設定等修正

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/items', function () {
+    return view('user.items');
+});
+
+require __DIR__ . '/auth.php';
