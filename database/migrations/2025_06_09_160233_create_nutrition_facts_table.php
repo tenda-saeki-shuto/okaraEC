3b992cc89e96fedb8ff9798485e84c3ec9c25758
tenda-yamaguchi-masaki <yamaguchi_masaki@tenda.co.jp>
2025-06-10 09:52:11 +0900

    resipeの栄養表示テーブル名変更

--- database/migrations/2025_06_09_160233_create_nutrition_facts_table.php
+++ database/migrations/2025_06_09_160233_create_nutrition_facts_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('nutrition_facts', function (Blueprint $table) {
+        Schema::create('recipe_nutrition_facts', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('recie_id');
             $table->foreign('recie_id')->references('id')->on('recipes');
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('nutrition_facts');
+        Schema::dropIfExists('recipe_nutrition_facts');
     }
 };
