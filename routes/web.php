0722b6ef2562932f2208db785c1d60980366fdd1
tenda_akasawa_natsumi <akasawa_natsumi@tenda.co.jp>
2025-06-11 11:06:41 +0900

    編集しました

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
+});
 
 Route::get('/admin/item', [ItemController::class, 'create'])->name('admin.item.create');
 Route::post('/admin/item', [ItemController::class, 'store'])->name('admin.item.store'); // 保存処理
