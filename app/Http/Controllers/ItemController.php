aca0f1b0cdc729ec905cf495594fdf3d2a7f63c0
tenda-yya <yamasaki_yuta@tenda.co.jp>
2025-06-12 13:09:46 +0900

    done item edit

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -81,13 +81,14 @@ class ItemController extends Controller
 
     private $item;
 
-    public function __construct(Item $item) {
+    public function __construct(Item $item)
+    {
         $this->item = $item;
     }
 
     public function index()
     { // 商品名、種類名、最終更新日、在庫
-        $item_list = Item::orderBy("id","desc")->paginate(20);
+        $item_list = Item::orderBy("id", "desc")->paginate(20);
         return view('admin.item_index', compact('item_list'));
     }
 
@@ -96,7 +97,7 @@ class ItemController extends Controller
         $item = $this->item;
         $categories = Category::all();
         $allergy_list = Allergy::all();
-        return view('admin.items_create', compact('item', 'categories', 'allergy_list'));
+        return view('admin.item_create', compact('item', 'categories', 'allergy_list'));
     }
 
     public function store(Request $request)
@@ -110,26 +111,26 @@ class ItemController extends Controller
             'price' => 'required|integer',
             'content' => 'nullable|string|max:255',
             'img' => 'required|string',
-            'category_id' => 'required|integer',
+            'category_id' => 'required|integer|exists:categories,id',
             'stock' => 'required|integer',
         ]);
 
         // 保存方法処理
         if ($request['is_cold'] == 'refrigerated') { // 冷蔵の時
             $itemData['is_cold'] = 1;
-        } else { // 冷凍の時
+        } else if ($request['is_cold'] == 'frozen') { // 冷凍の時
             $itemData['is_frozen'] = 1;
         }
         $item = Item::create($itemData);
 
         // table: item_nutrition_facts
         $nutritionData = $request->validate([
-            'energy' => 'required|integer',
-            'protein' => 'required|integer',
-            'fat' => 'required|integer',
-            'carb' => 'required|integer',
-            'fiber' => 'required|integer',
-            'salt_eqv' => 'required|integer',
+            'energy' => 'nullable|integer',
+            'protein' => 'nullable|integer',
+            'fat' => 'nullable|integer',
+            'carb' => 'nullable|integer',
+            'fiber' => 'nullable|integer',
+            'salt_eqv' => 'nullable|integer',
         ]);
 
         $nutritionData['item_id'] = $item->id;
@@ -139,7 +140,7 @@ class ItemController extends Controller
         // アレルギー
         // table: item_allergies
         $allergyIds = $request->validate([
-            'allergies.*' => 'nullable|integer',
+            'allergies.*' => 'nullable|integer|exists:allergies,id',
         ]);
         if (!empty($allergyIds)) {
             foreach ($allergyIds['allergies'] as $allergy_id) {
@@ -170,9 +171,110 @@ class ItemController extends Controller
         return redirect()->route('item.index');
     }
 
- 
     public function edit(Item $item)
     {
-        return view('admin.item_edit', compact('item'));
+        $item_nutrition_facts = $item->nutritionFacts;
+        $item_allergies = $item->itemAllergies()->pluck('id')->toArray();
+        if (!isset($item_allergies)) $item_allergies['id'] = null;
+        $categories = Category::all();
+        $allergy_list = Allergy::all();
+        return view('admin.item_edit', compact(
+            'item',
+            'item_nutrition_facts',
+            'item_allergies',
+            'categories',
+            'allergy_list'
+        ));
+    }
+
+    public function update(Request $request, Item $item)
+    {
+        //table: item, item_nutrition_facts(栄養), item_picture(サブ写真), item_allergies,
+
+        // 商品データ
+        // table: Items
+        $itemData = $request->validate([
+            'name' => 'required|max:30',
+            'price' => 'required|integer',
+            'content' => 'nullable|string|max:255',
+            // 'img' => 'required|string',
+            'category_id' => 'required|integer|exists:categories,id',
+            'stock' => 'required|integer',
+        ]);
+
+        // 保存方法処理
+        if ($request['is_cold'] == 'refrigerated') { // 冷蔵の時
+            $itemData['is_cold'] = 1;
+        } else if ($request['is_cold'] == 'frozen') { // 冷凍の時
+            $itemData['is_frozen'] = 1;
+        }
+        $item->update($itemData);
+
+        // table: item_nutrition_facts
+        $nutritionData = $request->validate([
+            'energy' => 'nullable|integer',
+            'protein' => 'nullable|integer',
+            'fat' => 'nullable|integer',
+            'carb' => 'nullable|integer',
+            'fiber' => 'nullable|integer',
+            'salt_eqv' => 'nullable|integer',
+        ]);
+
+        $nutritionData['item_id'] = $item->id;
+        $item->nutritionFacts()->update($nutritionData);
+
+        // アレルギー
+        // table: item_allergies
+        $allergyIds = $request->validate([
+            'allergies.*' => 'nullable|integer|exists:allergies,id',
+        ]);
+        $allergy_ids = $item->itemAllergies()->pluck('id')->toArray();
+        $form_allergy_ids = array_filter($allergyIds['allergies'] ?? []);
+        if (!empty($allergyIds)) {
+            foreach ($allergyIds['allergies'] as $index => $allergy_id) {
+                $id = $form_allergy_ids[$index] ?? null;
+                
+                if ($id) {
+                    $allergy = ItemAllergy::find($id);
+                    if ($allergy) {
+                        $allergy->update([
+                            'item_id' => $item->id,
+                            'allergy_id' => $allergy_id,
+                        ]);
+                    }
+                } else {
+                    ItemAllergy::create([
+                        'item_id' => $item->id,
+                        'allergy_id' => $allergy_id,
+                    ]);
+                }
+            }
+        }
+        $allergy_delete_ids = array_diff($allergy_ids, $form_allergy_ids);
+        if (!empty($allergy_delete_ids)) {
+            $item->itemAllergies()->whereIn('id', $allergy_delete_ids)->delete();
+        }
+        
+        // サブ画像
+        $subImgs = $request->validate([
+            'sub_imgs.*' => 'nullable|max:300',
+        ]);
+        $form_sub_img = array_filter($subImgs['sub_imgs'] ?? []);
+        if (!empty($form_sub_img)) {
+            $sub_img_ids = $item->itemPictures()->pluck('id')->toArray();
+            foreach ($subImgs['sub_imgs'] as $img) {
+                if (!is_null($img)) {
+                    ItemPicture::create([
+                        'item_id' => $item->id,
+                        'img' => $img,
+                    ]);
+                }
+            }
+            $item->itemPictures()->whereIn('id', $sub_img_ids)->delete();
+        }
+
+
+        $request->session()->flash('message', '更新しました');
+        return redirect()->route('item.index');
     }
 }
