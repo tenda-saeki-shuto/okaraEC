2078b9b5a694bd2d12b645cbc7a2bac8efca8189
tenda-yamaguchi-masaki <yamaguchi_masaki@tenda.co.jp>
2025-06-16 11:32:54 +0900

    コードの修正

--- database/seeders/UserOptSeeder.php
+++ database/seeders/UserOptSeeder.php
@@ -17,14 +17,13 @@ class UserOptSeeder extends Seeder
     {
         DB::table('addresses')->insert([
                 [
-                'user_id' => 1,
+                'user_id' => 100,
                 'postal_code' => '1234567',
                 'prefecture_id' => 2,
                 'address' => '〇〇〇〇市〇〇〇１－３－４',
             ],
         ]);
-    }
-}
+  
         // DB::table('user_likes')->insert([
         //     [
         //         'user_id' => 1,
@@ -50,56 +49,62 @@ class UserOptSeeder extends Seeder
         //         'payment' => 'クレジット',
         //     ],
         // ]);
-        // DB::table('orders')->insert([
-        //     [
-        //         'id' => 1,
-        //         'user_id' => 2,
-        //         'order_code' => 'XXXXXXXX',
-        //         'dateTime' => '2025-06-01 12:34',
-        //         'status' => '発送済み',
-        //         'is_regular' => false,
-        //         'payment' => 'クレジット',
-        //         'postal_code' => '1234567',
-        //         'prefecture' => '東京都',
-        //         'address' => '渋谷区渋谷〇〇〇ビル205号室',
-        //         'email' => 'XXXX@aaaa',
-        //         'tel' => '12345678901',
-        //     ],
-        //     [
-        //         'id' => 2,
-        //         'user_id' => 2,
-        //         'order_code' => 'XXXXXXXX',
-        //         'dateTime' => '2025-06-02 12:34',
-        //         'status' => '発送済み',
-        //         'is_regular' => true,
-        //         'payment' => 'クレジット',
-        //         'postal_code' => '1234567',
-        //         'prefecture' => '東京都',
-        //         'address' => '渋谷区渋谷〇〇〇ビル205号室',
-        //         'email' => 'XXXX@aaaa',
-        //         'tel' => '12345678901',
-        //     ],
-        // ]);
-        // DB::table('order_details')->insert([
-        //     [
-        //         'order_id' => 1,
-        //         'item_name' => 'おからクッキー',
-        //         'price' => 100,
-        //         'count' => 10,
-        //     ],
-        //     [
-        //         'order_id' => 1,
-        //         'item_name' => 'おからクッキー　いちご',
-        //         'price' => 100,
-        //         'count' => 3,
-        //     ],
-        //     [
-        //         'order_id' => 2,
-        //         'item_name' => 'おからクッキー　プレーン',
-        //         'price' => 100,
-        //         'count' => 4,
-        //     ],
-        // ]);
+        DB::table('orders')->insert([
+            [
+                'id' => 1,
+                'user_id' => 100,
+                'order_code' => '54321',
+                'dateTime' => '2025-06-01 12:34',
+                'status' => '発送済み',
+                'is_regular' => false,
+                'payment' => 'クレジット',
+                'postal_code' => '1234567',
+                'prefecture' => '東京都',
+                'address' => '渋谷区渋谷〇〇〇ビル205号室',
+                'email' => 'XXXX@aaaa',
+                'tel' => '12345678901',
+            ],
+            [
+                'id' => 2,
+                'user_id' => 100,
+                'order_code' => '12345',
+                'dateTime' => '2025-06-02 12:34',
+                'status' => '発送済み',
+                'is_regular' => true,
+                'payment' => 'クレジット',
+                'postal_code' => '1234567',
+                'prefecture' => '東京都',
+                'address' => '渋谷区渋谷〇〇〇ビル205号室',
+                'email' => 'XXXX@aaaa',
+                'tel' => '12345678901',
+            ],
+        ]);
+        DB::table('order_details')->insert([
+            [
+                'order_id' => 1,
+                'item_name' => 'おからクッキー',
+                'price' => 100,
+                'count' => 10,
+            ],
+            [
+                'order_id' => 1,
+                'item_name' => 'おからクッキー　いちご',
+                'price' => 100,
+                'count' => 3,
+            ],
+             [
+                'order_id' => 1,
+                'item_name' => '新規登録クーポン',
+                'price' => -100,
+                'count' => 3,
+            ],
+            [
+                'order_id' => 2,
+                'item_name' => 'おからクッキー　プレーン',
+                'price' => 100,
+                'count' => 4,
+            ],
+        ]);
         // DB::table('user_coupons')->insert([
         //     [
         //         'user_id' => 1,
@@ -118,4 +123,5 @@ class UserOptSeeder extends Seeder
         //         'is_clear' => true,
         //     ],
         // ]);
- 
+    }
+}
