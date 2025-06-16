b81655fabe2538589616dcc33f8355cf811a5e58
tenda_akasawa_natsumi <akasawa_natsumi@tenda.co.jp>
2025-06-17 08:54:28 +0900

    ユーザー情報編集

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,22 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+    <div class="py-12 bg-white">
+        <div class="max-w-5xl mx-auto px-6">
+            <h1 class="text-3xl font-semibold text-center border-b-4 border-yellow-700 pb-4 mb-10">
+                プロフィール編集
+            </h1>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
+            <div class="bg-white border border-gray-200 shadow-md rounded-xl p-8 mb-10">
+                {{-- ここにユーザー情報のテーブルや編集フォーム --}}
+                @include('profile.partials.update-profile-information-form')
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+                {{-- 例: パスワード変更フォームも同様に --}}
+                <div class="mt-10">
                     @include('profile.partials.update-password-form')
                 </div>
+
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
+            {{-- 必要なら他のリンクやアイコン群 --}}
         </div>
     </div>
 </x-app-layout>
